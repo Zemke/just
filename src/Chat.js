8bac23ae69a0f08286560aa255d41415a6b0c658
Zemke <florian@zemke.io>
2019-12-26 14:25:41 +0100

    Split components

---
+++ src/Chat.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+import DataStore from './dataStore';
+
+export default class AppComponent extends React.Component {
+
+  state = {field: '', messages: []};
+
+  onChange = async val => this.setState({field: val});
+
+  onSubmit = async e => {
+    e.preventDefault();
+    const payload = {
+      chat: 'epicChatId',
+      from: 'me',
+      to: 'lea',
+      body: this.state.field
+    };
+    await DataStore.sendMessage(payload);
+    this.setState({field: ''});
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Just</h1>
+          <form onSubmit={e => this.onSubmit(e)}>
+            <input onChange={e => this.onChange(e.target.value)}
+                   value={this.state.field}/>
+          </form>
+
+          <ul>
+            {this.state.messages.map(message =>
+              <li key={message.id}>
+                from: {message.from}<br/>
+                to: {message.to}<br/>
+                body: {message.body}<br/>
+                when: {message.when}
+              </li>
+            )}
+          </ul>
+        </header>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    // todo handle types (added, modified, removed)
+    DataStore.onMessage((message, type) =>
+      this.setState(state => ({messages: [...state.messages, message]})));
+  }
+}
