0bed09e1d575486a39d2f66b4fe7602abf06643f
Zemke <florian@zemke.io>
2019-12-27 22:11:30 +0100

    Reinstate chat switch

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import DataStore from './dataStore';
-import Auth from './auth';
 import otherUser from "./otherUser";
 
 export default class AppComponent extends React.Component {
@@ -36,20 +35,25 @@ export default class AppComponent extends React.Component {
   };
 
   render() {
+    const otherUsers = this.props.messages
+      .reduce((acc, m) => {
+        const otherUser1 = otherUser(this.props.currentUser.uid, [m]);
+        if (otherUser1 === this.state.otherUser) return acc;
+        if (acc.indexOf(otherUser1) === -1) acc.push(otherUser1);
+        return acc;
+      }, []);
+
     return (
       <div className="App">
         <header className="App-header">
           <button onClick={this.deleteChat}>Delete</button>
           {/*<button onClick={this.props.goToCreateChat}>Create</button>*/}
-          {/*<select onChange={e => this.props.goToChat(JSON.parse(e.target.value))}>*/}
-          {/*  <option defaultValue>Select chat</option>*/}
-          {/*  {this.props.chatss.map(chat =>*/}
-          {/*    <option key={chat.code}*/}
-          {/*            value={JSON.stringify(chat)}>*/}
-          {/*      {chat.name}*/}
-          {/*    </option>*/}
-          {/*  )}*/}
-          {/*</select>*/}
+          <select onChange={e => this.setState({otherUser: e.target.value})}>
+            <option defaultValue value={this.state.otherUser}>
+              {this.state.otherUser}
+            </option>
+            {otherUsers.map(oU => <option key={oU} value={oU}>{oU}</option>)}
+          </select>
           <h1>Just</h1>
           <form onSubmit={e => this.onSubmit(e)}>
             <input onChange={e => this.onChange(e.target.value)}
