49a835ec32499ce40846c4f125ae7262e8cb3259
Zemke <florian@zemke.io>
2019-12-27 23:37:38 +0100

    Improve sign in worklfow

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,20 +4,25 @@ import DataStore from "./dataStore";
 
 export default class SignInComponent extends React.Component {
 
-  state = {email: '', isSignInLink: false};
+  state = {email: '', isSignInLink: false, signedIn: null, emailSent: false};
 
   onSubmit = e => {
     e.preventDefault();
-    // todo show confirmation
     Auth
       .sendMail(this.state.email)
-      .then(() => DataStore.saveSignInEmail(this.state.email))
-      .catch(alert);
+      .then(()=> {
+        DataStore.saveSignInEmail(this.state.email);
+        this.setState({emailSent: true});
+      });
   };
 
   render() {
-    if (this.state.isSignInLink) {
+    if (this.state.signedIn) {
+      return <p>You are signed in with {this.state.signedIn.email}.</p>;
+    } else if (this.state.isSignInLink) {
       return <p>You are being signed in.</p>;
+    } else if (this.state.emailSent) {
+      return <p>You have been sent an email.</p>;
     }
     return (
       <form onSubmit={e => this.onSubmit(e)}>
@@ -32,16 +37,23 @@ export default class SignInComponent extends React.Component {
   componentDidMount() {
     Auth
       .current()
-      .then(console.log)
-      .catch(() => console.log("no auth"));
-
-    if (Auth.isSignInLink(window.location.href)) {
-      this.setState({isSignInLink: true});
-
-      Auth
-        .signIn(DataStore.getSignInEmail())
-        .then(() => DataStore.removeSignInEmail())
-        .catch(alert);
-    }
+      .then(user => {
+        if (user) {
+          console.log(user);
+          this.setState({signedIn: user});
+          window.history.pushState(null, null, '/');
+        } else if (Auth.isSignInLink(window.location.href)) {
+          this.setState({isSignInLink: true});
+          Auth
+            .signIn(DataStore.getSignInEmail())
+            .then(result => {
+              DataStore.removeSignInEmail();
+              window.history.pushState(null, null, '/');
+              this.setState({signedIn: result.user, isSignInLink: false});
+            });
+        } else {
+          window.history.pushState(null, null, '/');
+        }}
+      );
   }
 }
