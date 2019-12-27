5491a4209ad35d1f810868739f89680707c5a184
Zemke <florian@zemke.io>
2019-12-27 12:31:46 +0100

    Sign in check first

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Create from "./Create";
 import Chat from "./Chat";
 import DataStore from "./dataStore";
+import SignIn from "./SignIn";
+import Auth from "./auth";
 
 export default class AppComponent extends React.Component {
 
@@ -11,7 +13,9 @@ export default class AppComponent extends React.Component {
   goToChat = chat => this.setState({chat});
 
   render() {
-    if (this.state.chat != null) {
+    if (this.state.signIn) {
+      return <SignIn/>
+    } else if (this.state.chat != null) {
       return <Chat chat={this.state.chat}
                    goToCreateChat={() => this.setState({chat: null})}
                    goToChat={chat => this.setState({chat})}/>;
@@ -21,12 +25,16 @@ export default class AppComponent extends React.Component {
   }
 
   componentDidMount() {
-    Notification.requestPermission();
+    Auth.current()
+      .then(() => {
+        Notification.requestPermission();
 
-    if (this.state.chat != null) return;
-    const allChats = DataStore.getChats();
-    if (allChats != null && allChats.length) {
-      this.setState({chat: allChats[0]});
-    }
+        if (this.state.chat != null) return;
+        const allChats = DataStore.getChats();
+        if (allChats != null && allChats.length) {
+          this.setState({chat: allChats[0]});
+        }
+      })
+      .catch(() => this.setState({signIn: true}));
   }
 }
