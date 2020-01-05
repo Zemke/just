05cd7dc6fb4a6b4c23a1373ac6ba8fc02955b57c
Zemke <florian@zemke.io>
2020-01-05 15:48:39 +0100

    Migrate Chat to Hooks

--- src/Chat.js
+++ src/Chat.js
@@ -1,73 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Chat.css';
 import DataStore from './dataStore';
-import otherUser from "./otherUser";
+import extractOtherUser from "./otherUser";
 import ChatMenu from "./ChatMenu";
 import ChatSelect from "./ChatSelect";
 
-export default class AppComponent extends React.Component {
+export default function Chat(props) {
 
-  state = {
-    field: '',
-    otherUser: otherUser(
-      this.props.currentUser.uid,
-      this.props.messages
-        .sort((c1, c2) => c1 - c2))
-  };
+  const [field, setField] = useState('');
+  const [otherUser] = useState(extractOtherUser(
+    props.currentUser.uid, props.messages.sort((c1, c2) => c1 - c2)));
 
-  onChange = async val => this.setState({field: val});
-
-  onSubmit = async e => {
+  const onSubmit = async e => {
     e.preventDefault();
     const payload = {
-      from: this.props.currentUser.uid,
-      to: this.state.otherUser,
-      body: this.state.field
+      from: props.currentUser.uid,
+      to: otherUser,
+      body: field
     };
     await DataStore.sendMessage(payload);
-    this.setState({field: ''});
+    setField('');
   };
 
-  render() {
-    const otherUsers = this.props.messages
-      .reduce((acc, m) => {
-        const otherUser1 = otherUser(this.props.currentUser.uid, [m]);
-        if (otherUser1 === this.state.otherUser) return acc;
-        if (acc.indexOf(otherUser1) === -1) acc.push(otherUser1);
-        return acc;
-      }, []);
+  const otherUsers = props.messages
+    .reduce((acc, m) => {
+      const otherUser1 = extractOtherUser(props.currentUser.uid, [m]);
+      if (otherUser1 === otherUser) return acc;
+      if (acc.indexOf(otherUser1) === -1) acc.push(otherUser1);
+      return acc;
+    }, []);
 
-    return (
-      <div className="chat">
-        <div className="head">
-          <ChatMenu goToShareYourCode={this.props.goToShareYourCode}
-                    goToEnterAnotherCode={this.props.goToEnterAnotherCode}/>
-          <div className="changeChat">
-            <ChatSelect otherUsers={otherUsers} otherUser={this.state.otherUser}/>
-          </div>
-        </div>
-        <div className="body">
-          {this.props.messages
-            .filter(m =>
-              otherUser(this.props.currentUser.uid, [m]) === this.state.otherUser)
-            .sort((c1, c2) => c1.when - c2.when)
-            .map(message =>
-              <div key={message.id} className="message-wrapper">
-                <div className={"message " + (this.state.otherUser === message.from ? "from" : "to")}>
-                  <div className="overlay"/>
-                  <p>{message.body}</p>
-                </div>
-              </div>
-            )}
-        </div>
-        <div className="foot">
-          <form onSubmit={e => this.onSubmit(e)}>
-            <input onChange={e => this.onChange(e.target.value)}
-                   placeholder="Type here"
-                   value={this.state.field}/>
-          </form>
+  return (
+    <div className="chat">
+      <div className="head">
+        <ChatMenu goToShareYourCode={props.goToShareYourCode}
+                  goToEnterAnotherCode={props.goToEnterAnotherCode}/>
+        <div className="changeChat">
+          <ChatSelect otherUsers={otherUsers} otherUser={otherUser}/>
         </div>
       </div>
-    );
-  }
-}
+      <div className="body">
+        {props.messages
+          .filter(m =>
+            extractOtherUser(props.currentUser.uid, [m]) === otherUser)
+          .sort((c1, c2) => c1.when - c2.when)
+          .map(message =>
+            <div key={message.id} className="message-wrapper">
+              <div className={"message " + (otherUser === message.from ? "from" : "to")}>
+                <div className="overlay"/>
+                <p>{message.body}</p>
+              </div>
+            </div>
+          )}
+      </div>
+      <div className="foot">
+        <form onSubmit={e => onSubmit(e)}>
+          <input onChange={e => setField(e.target.value)}
+                 placeholder="Type here"
+                 value={field}/>
+        </form>
+      </div>
+    </div>
+  );
+};
