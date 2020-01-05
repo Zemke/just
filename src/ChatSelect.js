248f7f97a8e2c079357d46f43e44a1595d5fa5ea
Zemke <florian@zemke.io>
2020-01-05 15:39:14 +0100

    Only select other users when available

--- src/ChatSelect.js
+++ src/ChatSelect.js
@@ -5,16 +5,19 @@ export default function ChatSelect(props) {
   const [expanded, setExpanded] = useState(false);
 
   return (<>
-    <div className="otherUser" onClick={() => setExpanded(!expanded)}>
+    <div className="otherUser"
+         onClick={() => props.otherUsers.length && setExpanded(!expanded)}>
       {toName(props.otherUser)}
     </div>
-    <div className={'dropdown attach-left' + (expanded ? ' expanded' : '')}>
-      <ul>
-        {props.otherUsers.map(user =>
-          <li key={user} onChange={() => props.onSelect(user)}>
-            {toName(user)}
-          </li>)}
-      </ul>
-    </div>
+    {!!props.otherUsers.length && (
+      <div className={'dropdown attach-left' + (expanded ? ' expanded' : '')}>
+        <ul>
+          {props.otherUsers.map(user =>
+            <li key={user} onChange={() => props.onSelect(user)}>
+              {toName(user)}
+            </li>)}
+        </ul>
+      </div>
+    )}
   </>)
 }
