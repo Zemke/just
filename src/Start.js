bb20284a144023d1e52c9f3364a8557acea30dee
Zemke <florian@zemke.io>
2020-01-07 21:32:34 +0100

    Looks of Start

--- src/Start.js
+++ src/Start.js
@@ -3,18 +3,22 @@ import React from 'react';
 export default class StartComponent extends React.Component {
 
   render() {
-    return (<>
-      <h1>Just</h1>
+    return (
+      <div className="translucent translucent-center text-center">
+        <h1>Just</h1>
 
-      The messenger to message people.
+        <p>To message somebody you need to either enter their code or share your code.</p>
+        <p>Be careful who you share your code with. Anybody who has it can message you.</p>
 
-      <button onClick={this.props.shareYourCode}>
-        Share your code
-      </button>
-
-      <button onClick={this.props.enterAnotherCode}>
-        Enter another code
-      </button>
-    </>);
+        <div className="margin-top">
+          <button onClick={this.props.shareYourCode} className="form-control">
+            Share your code
+          </button>
+          <button onClick={this.props.enterAnotherCode} className="form-control">
+            Enter another code
+          </button>
+        </div>
+      </div>
+    );
   }
 }
