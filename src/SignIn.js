14586606519fae06b7338afe85889d400cdf8bbf
Zemke <florian@zemke.io>
2020-01-07 11:57:23 +0100

    Migrate SignIn to Hooks

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,59 +1,63 @@
-import * as React from "react";
+import React, {useEffect, useState} from "react";
 import Auth from './auth';
 import DataStore from "./dataStore";
 
-export default class SignInComponent extends React.Component {
+export default function SignInComponent(props) {
 
-  state = {email: '', isSignInLink: false, signedIn: null, emailSent: false};
+  const [email, setEmail] = useState('');
+  const [isSignInLink, setIsSignInLink] = useState(false);
+  const [signedIn, setSignedIn] = useState(null);
+  const [emailSent, setEmailSent] = useState(false);
 
-  onSubmit = e => {
-    e.preventDefault();
-    Auth
-      .sendMail(this.state.email)
-      .then(()=> {
-        DataStore.saveSignInEmail(this.state.email);
-        this.setState({emailSent: true});
-      });
-  };
-
-  render() {
-    if (this.state.signedIn) {
-      return <p>You are signed in with {this.state.signedIn.email}.</p>;
-    } else if (this.state.isSignInLink) {
-      return <p>You are being signed in.</p>;
-    } else if (this.state.emailSent) {
-      return <p>You have been sent an email.</p>;
-    }
-    return (
-      <form onSubmit={e => this.onSubmit(e)}>
-        <input
-          value={this.state.email}
-          onChange={e => this.setState({email: e.target.value})}/>
-        <button type="submit">Sign In</button>
-      </form>
-    );
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     Auth
       .current()
       .then(user => {
-        if (user) {
-          this.setState({signedIn: user});
-          window.history.pushState(null, null, '/');
-        } else if (Auth.isSignInLink(window.location.href)) {
-          this.setState({isSignInLink: true});
-          Auth
-            .signIn(DataStore.getSignInEmail())
-            .then(result => {
-              DataStore.removeSignInEmail();
-              window.history.pushState(null, null, '/');
-              this.setState({signedIn: result.user, isSignInLink: false});
-              this.props.signedIn(result.user);
-            });
-        } else {
-          window.history.pushState(null, null, '/');
-        }}
+          if (user) {
+            setSignedIn(user);
+            window.history.pushState(null, null, '/');
+          } else if (Auth.isSignInLink(window.location.href)) {
+            setIsSignInLink(true);
+            Auth
+              .signIn(DataStore.getSignInEmail())
+              .then(result => {
+                DataStore.removeSignInEmail();
+                window.history.pushState(null, null, '/');
+                setSignedIn(result.user);
+                setIsSignInLink(false);
+                props.signedIn(result.user);
+              });
+          } else {
+            window.history.pushState(null, null, '/');
+          }
+        }
       );
+  });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    Auth
+      .sendMail(email)
+      .then(() => {
+        DataStore.saveSignInEmail(email);
+        setEmailSent(true);
+      });
+  };
+
+  if (signedIn) {
+    return <p>You are signed in with {signedIn.email}.</p>;
+  } else if (isSignInLink) {
+    return <p>You are being signed in.</p>;
+  } else if (emailSent) {
+    return <p>You have been sent an email.</p>;
   }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={email}
+        onChange={e => setEmail(e.target.value)}/>
+      <button type="submit">Sign In</button>
+    </form>
+  );
 }
