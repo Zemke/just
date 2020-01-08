d243a178eff3349e728d9806f37a59696b9ab7cc
Zemke <florian@zemke.io>
2020-01-08 21:49:39 +0100

    Migrate Start to Hooks

--- src/Start.js
+++ src/Start.js
@@ -1,24 +1,22 @@
 import React from 'react';
 
-export default class StartComponent extends React.Component {
+export default function Start(props) {
 
-  render() {
-    return (
-      <div className="translucent translucent-center text-center">
-        <h1>Just</h1>
+  return (
+    <div className="translucent translucent-center text-center">
+      <h1>Just</h1>
 
-        <p>To message somebody you need to either enter their code or share your code.</p>
-        <p>Be careful who you share your code with. Anybody who has it can message you.</p>
+      <p>To message somebody you need to either enter their code or share your code.</p>
+      <p>Be careful who you share your code with. Anybody who has it can message you.</p>
 
-        <div className="margin-top">
-          <button onClick={this.props.shareYourCode} className="form-control">
-            Share your code
-          </button>
-          <button onClick={this.props.enterAnotherCode} className="form-control">
-            Enter another code
-          </button>
-        </div>
+      <div className="margin-top">
+        <button onClick={props.shareYourCode} className="form-control">
+          Share your code
+        </button>
+        <button onClick={props.enterAnotherCode} className="form-control">
+          Enter another code
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
