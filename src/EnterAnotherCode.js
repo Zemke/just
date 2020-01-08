d558e74030be7f4f614ce550f80230fd89d085ff
Zemke <florian@zemke.io>
2020-01-08 22:04:43 +0100

    Looks of EnterAnotherCode

--- src/EnterAnotherCode.js
+++ src/EnterAnotherCode.js
@@ -16,14 +16,25 @@ export default function EnterAnotherCode(props) {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input value={anotherCode}
-             placeholder="The other person’s code"
-             onChange={e => setAnotherCode(e.target.value)}/>
-      <input value={firstMessage}
-             placeholder="Your first message"
-             onChange={e => setFirstMessage(e.target.value)}/>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="translucent translucent-center text-center">
+      <h1>Just</h1>
+      <p>
+        Enter the code you have received here
+        and introduce yourself in an initial message.
+      </p>
+      <form onSubmit={onSubmit}>
+        <input value={anotherCode}
+               className="form-control text-center"
+               placeholder="The other person’s code"
+               onChange={e => setAnotherCode(e.target.value)}/>
+        <input value={firstMessage}
+               className="form-control text-center"
+               placeholder="Your first message"
+               onChange={e => setFirstMessage(e.target.value)}/>
+        <button type="submit" className="form-control">
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
