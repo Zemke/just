79f3aff98b416183e6e75452fdb368b3736a8cff
Zemke <florian@zemke.io>
2020-01-19 12:15:28 +0100

    Check support for messaging

--- public/append-sw.js
+++ public/append-sw.js
@@ -11,19 +11,21 @@ self.addEventListener('notificationclick', e => {
 importScripts('https://www.gstatic.com/firebasejs/7.6.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.6.2/firebase-messaging.js');
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCpeA-4i6sZalkiqjB3ks6u1__hO4E2o8U",
-  authDomain: "just-pwa.firebaseapp.com",
-  databaseURL: "https://just-pwa.firebaseio.com",
-  projectId: "just-pwa",
-  storageBucket: "just-pwa.appspot.com",
-  messagingSenderId: "389806956797",
-  appId: "1:389806956797:web:18d5c9ae865eda5b51de94",
-  measurementId: "G-8FFPRPW39V"
-});
+if (firebase.messaging.isSupported()) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCpeA-4i6sZalkiqjB3ks6u1__hO4E2o8U",
+    authDomain: "just-pwa.firebaseapp.com",
+    databaseURL: "https://just-pwa.firebaseio.com",
+    projectId: "just-pwa",
+    storageBucket: "just-pwa.appspot.com",
+    messagingSenderId: "389806956797",
+    appId: "1:389806956797:web:18d5c9ae865eda5b51de94",
+    measurementId: "G-8FFPRPW39V"
+  });
 
-const messaging = firebase.messaging();
+  const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(({data}) =>
-  self.registration.showNotification(
-    data.fromName, {body: data.body, badge: '/logo72.png', icon: '/logo192.png'}));
+  messaging.setBackgroundMessageHandler(({data}) =>
+    self.registration.showNotification(
+      data.fromName, {body: data.body, badge: '/logo72.png', icon: '/logo192.png'}));
+}
--- src/util/messaging.js
+++ src/util/messaging.js
@@ -1,18 +1,19 @@
 import * as firebase from "firebase/app";
 import "firebase/messaging";
 
-const messaging = firebase.messaging();
-
-messaging.usePublicVapidKey("BAoM6C4OkVC0TASZfRYD_I_es2VHdg8E_5owQWlza2sS79FFhfu5SN2bcueGyvZ9WBzyS4AiTeFLXutCLKyskeQ");
-
-const getToken = () => messaging.getToken();
-const onTokenRefresh = cb => messaging.onTokenRefresh(() => messaging.getToken().then(cb));
-const onMessage = cb => messaging.onMessage(cb);
-
 export default (async () => {
+  if (!firebase.messaging.isSupported()) return null;
   if (!('serviceWorker' in navigator)) return null;
   const reg = await navigator.serviceWorker.getRegistration();
   if (!reg) return null;
+
+  const messaging = firebase.messaging();
+  messaging.usePublicVapidKey("BAoM6C4OkVC0TASZfRYD_I_es2VHdg8E_5owQWlza2sS79FFhfu5SN2bcueGyvZ9WBzyS4AiTeFLXutCLKyskeQ");
   messaging.useServiceWorker(reg);
+
+  const getToken = () => messaging.getToken();
+  const onTokenRefresh = cb => messaging.onTokenRefresh(() => messaging.getToken().then(cb));
+  const onMessage = cb => messaging.onMessage(cb);
+
   return {getToken, onTokenRefresh, onMessage};
 })();
