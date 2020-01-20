89b550c5698901f5ecc77953dfaa94db9e4643f3
Zemke <florian@zemke.io>
2020-01-21 00:27:51 +0100

    No smooth scroll on input resize

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -138,6 +138,7 @@ export default function Chat(props) {
   const onInputFieldResize = height => {
     if (!chatBodyEl.current) return;
     chatBodyEl.current.style.marginBottom = height + 'px';
+    chatEl.current.classList.remove('scrollSmooth');
     scrollToBottom();
   };
 
