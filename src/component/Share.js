b0c93a8c7ca92eef9e68acefb5f86be14088d57a
Zemke <florian@zemke.io>
2020-01-25 11:25:57 +0100

    Share dropdown

--- src/component/Share.js
+++ src/component/Share.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import Dropdown from "./Dropdown";
 
 export default function Share() {
 
+  const [dropdownTrigger, setDropdownTrigger] = useState(null);
+
+  const takePhoto = () => {
+
+  };
+
+  const imageGallery = () => {
+
+  };
+
   return (
-    <div className="share">
-      <button>&#43;</button>
-      <input type="file" accept="image/x-png,image/jpeg,image/gif" className="share" />
-    </div>
+    <>
+      <div className="share">
+        <button ref={ref => setDropdownTrigger(ref)}>&#43;</button>
+        <input type="file" accept="image/x-png,image/jpeg,image/gif" className="share"/>
+      </div>
+      <Dropdown dropdownTrigger={dropdownTrigger} className="attachBottomLeft text-left">
+        <ul>
+          <li onClick={takePhoto}>
+            📷 Take photo
+          </li>
+          <li onClick={imageGallery}>
+            🌉 Image gallery
+          </li>
+        </ul>
+      </Dropdown>
+    </>
   )
 }
