1b0d390e098423f6c29e835ceb07831ba162d371
Zemke <florian@zemke.io>
2020-01-25 15:25:21 +0100

    Adjustments

--- src/component/Share.js
+++ src/component/Share.js
@@ -1,31 +1,33 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import Dropdown from "./Dropdown";
 
 export default function Share() {
 
   const [dropdownTrigger, setDropdownTrigger] = useState(null);
+  /** @type {{current: HTMLInputElement}} */ const uploadButton = useRef(null);
 
   const takePhoto = () => {
-
+    alert('todo');
   };
 
   const imageGallery = () => {
-
+    uploadButton.current.click();
+    console.log(uploadButton.current.files);
   };
 
   return (
     <>
       <div className="share">
         <button ref={ref => setDropdownTrigger(ref)}>&#43;</button>
-        <input type="file" accept="image/x-png,image/jpeg,image/gif"/>
+        <input type="file" accept="image/x-png,image/jpeg,image/gif" ref={uploadButton}/>
       </div>
       <Dropdown dropdownTrigger={dropdownTrigger} className="attachBottomLeft text-left">
         <ul>
-          <li onClick={takePhoto}>
-            <span className="icon">📷</span> Take photo
-          </li>
           <li onClick={imageGallery}>
-            <span className="icon">🌉</span> Image gallery
+            <span className="icon">🌉</span> Gallery
+          </li>
+          <li onClick={takePhoto}>
+            <span className="icon">📷</span> Camera
           </li>
         </ul>
       </Dropdown>
