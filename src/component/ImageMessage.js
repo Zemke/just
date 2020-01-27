c3b47f3f0ae61fa6ff126cfb67ee8b9a27d0a2eb
Zemke <florian@zemke.io>
2020-01-27 23:03:45 +0100

    Fix image loading indication

--- src/component/ImageMessage.js
+++ src/component/ImageMessage.js
@@ -1,23 +1,28 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Storage from '../util/storage.js';
 import './ImageMessage.css';
 
 export default function ImageMessage(props) {
 
   const imageRef = useRef(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       if (imageRef.current == null) return;
       imageRef.current.src = await Storage.download(props.message.image);
+      setLoading(false);
     })();
   }, [props.message]);
 
   return (
     <>
-      {(imageRef?.current?.src)
-        ? (<img alt="Image message" ref={imageRef}/>)
-        : (<div className="loadingImage">Loading</div>)}
+      <img alt="Image message"
+           ref={imageRef}
+           className={loading ? 'display-none' : ''}/>
+      <div className={'loadingImage' + (loading ? '' : ' display-none')}>
+        Loading
+      </div>
     </>
   )
 };
