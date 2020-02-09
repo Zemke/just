20dd28d6343b733eb456332c5ff6df391e732953
Zemke <florian@zemke.io>
2020-02-09 16:32:17 +0100

    There's no loading image
    Images do never need to be in a loading state.
    
    When the user is sending images it can be displayed right away as he's
    already having that image.
    When the user is receiving an image we just don't display anything
    until the image is ready to be displayed.
    
    So, there's not requirement for a loading sequence.

--- src/component/ImageMessage.js
+++ src/component/ImageMessage.js
@@ -6,25 +6,15 @@ export default function ImageMessage(props) {
 
   /** @type {{current: HTMLDivElement}} */ const elemRef = useRef(null);
 
-  const [loading, setLoading] = useState(null);
-
-  // todo loading indication on receiving
-  //  (between message received and image downloaded)
-
   useEffect(() => {
-    (async () => {
-      const currElemRef = elemRef.current;
-      if (!currElemRef) return;
+    const currElemRef = elemRef.current;
+    if (!currElemRef) return;
 
-      let imageSrc;
-      if (props.message.placeholder) {
-        imageSrc = URL.createObjectURL(props.message.placeholder);
-        setLoading(false);
-      } else {
-        setTimeout(() => setLoading(curr => curr === null ? true : curr), 1000);
-        imageSrc = await Storage.download(props.message.image);
-        setLoading(false);
-      }
+    (async () => {
+      const imageSrc = props.message.placeholder
+        ? URL.createObjectURL(props.message.placeholder)
+        : await Storage.download(props.message.image);
+
       const imgEl = document.createElement('img');
       imgEl.src = imageSrc;
       currElemRef.innerHTML = '';
@@ -35,7 +25,6 @@ export default function ImageMessage(props) {
   return (
     <div className={'image' + (props.message.placeholder ? ' sending' : '')}>
       <div ref={elemRef}/>
-      {loading === true && (<div className="loadingImage">Loading</div>)}
       {props.message.placeholder && (<div className="status">Sending</div>)}
     </div>
   )
